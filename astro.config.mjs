a13fb9779c974dd7c2f9c906a60ac08fc9096301
Albin Sander <51294735+Albin-Sander@users.noreply.github.com>
2026-05-10 17:48:32 +0200

    Fix css

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
   site: "https://github.com/Albin-Sander/stuns-help",
+  base: "/stuns-help",
   vite: {
     plugins: [tailwindcss()],
   },
